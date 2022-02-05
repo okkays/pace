457114879ebb3fa3a071943f4953e48b07aa3c46
Kevin Kays <kkays@raybeam.com>
2022-02-04 23:19:58 -0500

    remove bad values on entry of a valid metric

--- pace-app/src/app/app.component.ts
+++ pace-app/src/app/app.component.ts
@@ -6,32 +6,32 @@ import {Action} from './models/action';
 import {MaybeMetric, Metric} from './models/metric';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector : 'app-root',
+  templateUrl : './app.component.html',
+  styleUrls : [ './app.component.css' ]
 })
 export class AppComponent {
   actionSelected$ = new ReplaySubject<Action>(1);
   fromSubject$ = new ReplaySubject<Metric[]>(1);
   toSubject$ = new ReplaySubject<Metric[]>(1);
 
-  convertedMetric$: Observable<Metric> =
-      combineLatest([this.fromSubject$, this.toSubject$])
-          .pipe(
-              filter(([from, to]) => {
-                if (from.length !== 1 || to.length !== 1) return false;
-                if (to[0].unit === null) return false;
-                return true;
-              }),
-              map(([from, to]) => {
-                // Cast checked in filter above.
-                return from[0].toUnit(to[0].unit as string);
-              }),
-              tap(result => {
-                console.log('Result:', result);
-              }),
-              filter<MaybeMetric, Metric>(isMetricForFilter),
-          );
+  convertedMetric$: Observable<Metric|null> =
+      combineLatest([ this.fromSubject$, this.toSubject$ ])
+          .pipe(map(([ from, to ]) => {
+                  if (from.length !== 1 || to.length !== 1)
+                    return [ null, null ];
+                  if (to[0].unit === null)
+                    return [ null, null ];
+                  return [ from, to ];
+                }),
+                map(([ from, to ]) => {
+                  // Cast checked in filter above.
+                  if (!from || !to)
+                    return null;
+                  return from[0].toUnit(to[0].unit as string);
+                }),
+                tap(result => { console.log('Result:', result); }),
+                map(metric => metric?.isValid() ? metric : null));
 }
 
 function isMetricForFilter(metric: MaybeMetric): metric is Metric {
