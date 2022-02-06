dfe85b7b6779116d3539389a19bb4437d381fec4
Kevin Kays <kkays@raybeam.com>
2022-02-05 19:27:26 -0500

    add more conversion boxes when selecting a result

--- pace-app/src/app/app.component.ts
+++ pace-app/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {Observable, scan, Subject} from 'rxjs';
+import {startWith, tap} from 'rxjs/operators';
+
+import {Metric} from './models/metric';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +10,14 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  conversionsSubject = new Subject<Metric>();
+  conversions$: Observable<Array<Metric|null>> = this.conversionsSubject.pipe(
+      scan<Metric|null, Array<Metric|null>>(
+          (conversions, newConversion) => {
+            return [...conversions, newConversion];
+          },
+          [null]),
+      startWith([null]),
+      tap(conversion => console.log(conversion)),
+  );
 }
