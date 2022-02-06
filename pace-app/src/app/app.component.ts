71412ca0889c719de2385b8e2f319295f2b5dfff
Kevin Kays <kkays@raybeam.com>
2022-02-05 22:14:46 -0500

    scroll into view

--- pace-app/src/app/app.component.ts
+++ pace-app/src/app/app.component.ts
@@ -1,17 +1,21 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, QueryList, ViewChildren} from '@angular/core';
 import {merge, Observable, scan, Subject} from 'rxjs';
-import {map, mergeWith, startWith, tap} from 'rxjs/operators';
-
+import {map, startWith, takeUntil} from 'rxjs/operators';
 import {Metric} from './models/metric';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy {
   conversionsSubject = new Subject<Metric>();
   deletedSubject = new Subject<Metric>();
+  destroySubject = new Subject<void>();
+  @ViewChildren('conversion', {read: ElementRef})
+  conversionElements!: QueryList<ElementRef>;
+
   conversions$: Observable<Array<Metric|null>> =
       merge(
           this.conversionsSubject.pipe(map(added => ({added}))),
@@ -30,4 +34,31 @@ export class AppComponent {
                   [null]),
               startWith([null]),
           );
+
+  ngAfterViewInit() {
+    this.conversionElements.changes
+        .pipe(
+            // Cast because angular doesn't type the changes observable.
+            map(elements => elements as QueryList<ElementRef<HTMLElement>>),
+            scan(
+                (previousElements, currentElements) => {
+                  const addedElements = currentElements.filter(
+                      currentElement => !previousElements.some(
+                          previousElement =>
+                              currentElement === previousElement));
+                  if (addedElements.length) {
+                    addedElements[0].nativeElement.scrollIntoView(
+                        {block: 'start', behavior: 'smooth'});
+                    console.log('Element added, scrolling...');
+                  }
+                  return currentElements.toArray();
+                },
+                new Array<ElementRef<HTMLElement>>()),
+            takeUntil(this.destroySubject))
+        .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject.next();
+  }
 }
